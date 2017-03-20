89d948e6b474ad975d919564238d24fe3adcba55
Weiming Zhai <wmzhai@gmail.com>
2017-03-20 15:34:25 +0800

    animation

--- projects/tech_stack/src/components/list_item.js
+++ projects/tech_stack/src/components/list_item.js
@@ -1,19 +1,30 @@
 import React, { Component } from 'react';
-import { Text, TouchableWithoutFeedback, View } from 'react-native';
+import { 
+  Text,
+  TouchableWithoutFeedback,
+  View,
+  LayoutAnimation
+} from 'react-native';
 import { connect } from 'react-redux';
 import { CardSection } from './common';
 import * as actions from '../actions';
 
 class ListItem extends Component {
+  componentWillUpdate() {
+    LayoutAnimation.spring();
+  }
+
   renderDescription() {
-    const { library, selectedLibraryId } = this.props;
+    const { library, expanded } = this.props;
     console.log(this.props);
 
-    if (library.id === selectedLibraryId) {
+    if (expanded) {
       return (
-        <Text>
-          {library.description}
-        </Text>
+        <CardSection>
+          <Text style={{flex:1}} >
+            {library.description}
+          </Text>
+        </CardSection>
       );
     }
   }
@@ -46,8 +57,9 @@ const styles = {
   }
 };
 
-const mapStateToProps = state => {
-  return { selectedLibraryId: state.selectedLibraryId };
+const mapStateToProps = (state, ownProps) => {
+  const expanded = state.selectedLibraryId === ownProps.library.id;
+  return { expanded };
 };
 
 export default connect(mapStateToProps, actions)(ListItem);
