73a2433998922836f0a9cf5b4c76d6b91d7d24fe
Weiming Zhai <wmzhai@gmail.com>
2017-03-20 10:23:38 +0800

    add reusable components

--- projects/tech_stack/App.js
+++ projects/tech_stack/App.js
@@ -4,14 +4,14 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './src/reducers';
 
+import { Header } from './src/components/common';
+
 class App extends React.Component {
   render() {
     return (
       <Provider store={createStore(reducers)}>
-        <View style={styles.container}>
-          <Text>Open up App.js to start working on your app!</Text>
-          <Text>Changes you make will automatically reload.</Text>
-          <Text>Shake your phone to open the developer menu.</Text>
+        <View>
+          <Header headerText="Tech Stack"/>
         </View>
       </Provider>
     );
