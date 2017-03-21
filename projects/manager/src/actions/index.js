3bb869279828658b4e5b3539ad50be9325c27dd1
Weiming Zhai <wmzhai@gmail.com>
2017-03-21 11:15:26 +0800

    group scene with buckets

--- projects/manager/src/actions/index.js
+++ projects/manager/src/actions/index.js
@@ -1,4 +1,5 @@
 import firebase from 'firebase';
+import { Actions } from 'react-native-router-flux';
 import {
   EMAIL_CHANGED,
   PASSWORD_CHANGED,
@@ -24,7 +25,7 @@ export const passwordChanged = (text) => {
 export const loginUser = ({ email, password }) => {
   return (dispatch) => {
     dispatch({ type: LOGIN_USER });
-    
+
     firebase.auth().signInWithEmailAndPassword(email, password)
       .then(user => loginUserSuccess(dispatch, user))
       .catch(() => {
@@ -40,6 +41,8 @@ const loginUserSuccess = (dispatch, user) => {
     type: LOGIN_USER_SUCCESS,
     payload: user 
   });
+
+  Actions.main();
 };
 
 const loginUserFail = (dispatch) => {
