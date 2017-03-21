95c16b1607cd9715fc042b741734e1d5541fa219
Weiming Zhai <wmzhai@gmail.com>
2017-03-21 12:40:31 +0800

    create employee form

--- projects/manager/src/components/employee_create.js
+++ projects/manager/src/components/employee_create.js
@@ -1,14 +1,48 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { employeeUpdate } from '../actions';
+import { Card, CardSection, Input, Button } from './common';
 import { View, Text } from 'react-native';
 
 class EmployeeCreate extends Component {
   render() {
     return (
-      <View>
-        <Text>Employee Form</Text>
-      </View>
+      <Card>
+        <CardSection>
+          <Input
+            label="Name"
+            placeholder="Jane"
+            value={this.props.name}
+            onChangeText={text => this.props.employeeUpdate({ prop: 'name', value: text })}
+          />
+        </CardSection>
+
+        <CardSection> 
+          <Input
+            label="Phone"
+            placeholder="1380000000"
+            value={this.props.phone}
+            onChangeText={text => this.props.employeeUpdate({ prop: 'phone', value: text })}
+          />
+        </CardSection>
+        
+        <CardSection> 
+        </CardSection>
+        
+        <CardSection> 
+          <Button>
+            Create
+          </Button>
+        </CardSection>
+      </Card>
     );
   }
 };
 
-export default EmployeeCreate;
+const mapStateToProps = (state) => {
+  const { name, phone, shift } = state.employeeForm;
+
+  return { name, phone, shift };
+};
+
+export default connect(mapStateToProps, { employeeUpdate })(EmployeeCreate);
--- projects/manager/src/reducers/index.js
+++ projects/manager/src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import AuthReducer from './auth_reducer';
+import EmployeeFormReducer from './employee_form_reducer';
 
 export default combineReducers({
-  auth: AuthReducer
+  auth: AuthReducer,
+  employeeForm: EmployeeFormReducer
 });
