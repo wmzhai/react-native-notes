de4f59319a1cecfc70ae0b0037c01ef448150b2d
Weiming Zhai <wmzhai@gmail.com>
2017-03-21 14:42:24 +0800

    finish 143 but save failed

--- projects/manager/src/components/employee_create.js
+++ projects/manager/src/components/employee_create.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 import { Picker, Text } from 'react-native';
 import { connect } from 'react-redux';
-import { employeeUpdate } from '../actions';
+import { employeeUpdate, employeeCreate } from '../actions';
 import { Card, CardSection, Input, Button } from './common';
 
 class EmployeeCreate extends Component {
+
+  onButtonPress() {
+    const { name , phone, shift } = this.props;
+    this.props.employeeCreate({ name , phone, shift });
+  }
+
   render() {
     return (
       <Card>
@@ -44,7 +50,7 @@ class EmployeeCreate extends Component {
         </CardSection>
         
         <CardSection> 
-          <Button>
+          <Button onPress={this.onButtonPress.bind(this)}>
             Create
           </Button>
         </CardSection>
@@ -63,7 +69,9 @@ const styles = {
 const mapStateToProps = (state) => {
   const { name, phone, shift } = state.employeeForm;
 
-  return { name, phone, shift };
+  return { name, phone, shift: shift || 'Monday' };
 };
 
-export default connect(mapStateToProps, { employeeUpdate })(EmployeeCreate);
+export default connect(mapStateToProps, {
+  employeeUpdate, employeeCreate
+})(EmployeeCreate);
--- projects/manager/src/router.js
+++ projects/manager/src/router.js
@@ -7,10 +7,10 @@ import EmployeeCreate from './components/employee_create';
 const RouterComponent = () => {
   return (
     <Router sceneStyle={{ paddingTop: 65 }}>
-      <Scene key="auth">
+      <Scene key="auth" initial>
         <Scene key="login" component={LoginForm} title="Please Login" />
       </Scene>
-      <Scene key="main" initial >
+      <Scene key="main"  >
         <Scene
           onRight={() => Actions.employeeCreate()}
           rightTitle="Add"
