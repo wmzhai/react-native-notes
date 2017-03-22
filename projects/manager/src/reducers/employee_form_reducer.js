7f6833c351718749efcc57010e115b1e47beec8a
Weiming Zhai <wmzhai@gmail.com>
2017-03-22 08:25:15 +0800

    edit

--- projects/manager/src/reducers/employee_form_reducer.js
+++ projects/manager/src/reducers/employee_form_reducer.js
@@ -1,6 +1,7 @@
 import {
   EMPLOYEE_UPDATE,
-  EMPLOYEE_CREATE
+  EMPLOYEE_CREATE,
+  EMPLOYEE_SAVE_SUCCESS
 } from '../actions/types';
 
 const INITIAL_STATE = {
@@ -10,13 +11,14 @@ const INITIAL_STATE = {
 };
 
 export default (state = INITIAL_STATE, action) => {
-  switch(action.type) {
+  switch (action.type) {
     case EMPLOYEE_UPDATE:
-      return { ...state, [action.payload.prop]: action.payload.value }
+      return { ...state, [action.payload.prop]: action.payload.value };
     case EMPLOYEE_CREATE:
+      return INITIAL_STATE;
+    case EMPLOYEE_SAVE_SUCCESS:
       return INITIAL_STATE;
     default:
       return state;
   }
 };
-
